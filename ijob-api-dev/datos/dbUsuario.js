851ccce76c44cac869751c185be084dfe585cb09
Damian <aldacap@gmail.com>
2015-04-22 22:15:18 -0500

    db

--- ijob-api-dev/datos/dbUsuario.js
+++ ijob-api-dev/datos/dbUsuario.js
@@ -3,18 +3,20 @@
  */
 
 function dbUsuario() {
-    this.Usuario = require('../modelos/usuario');
+    var usuario = require('../modelos/usuario');
+    
+    this.autenticar = function (correo, clave, response) {
+        //var usuario = require('../modelos/usuario');
+        usuario.findOne({ correo: correo , clave: clave }, 'nombre apellidos token', function (err, usuario) {
+            
+            if (err) { return response.send(err); }
+            response.json(usuario);
+        //return usuario;
+        });
+    }
 }
 
-dbUsuario.prototype.autenticar = function (correo, clave, response) {
-    var Usuario = require('../modelos/usuario');
-    Usuario.findOne({ correo: correo , clave: clave }, 'nombre apellidos token', function (err, usuario) {
-        
-        if (err) { return res.send(err); }
-        response.json(usuario);
-
-        //return usuario;
-    });
-};
+//dbUsuario.prototype.
+//};
 
 module.exports = dbUsuario;
---
+++ ijob-api-dev/servicios/usuarios.js
@@ -0,0 +1,23 @@
+﻿/**
+ *  api para los usuarios
+ */
+
+var express = require('express');
+var router = express.Router();
+var uuid = require('uuid'); // modulo para generar los token
+
+var dbUsuario = require('../datos/dbUsuario');
+var dbUsr = new dbUsuario();
+
+// autentica un usuario mediante correo y clave
+router.route('/autenticar/:correo/:clave')
+    .get(function (req, res) {
+    
+    
+    dbUsr.autenticar(req.params.correo, req.params.clave, res);
+    
+   // res.json(ret);
+
+});
+
+module.exports = router;
